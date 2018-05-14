89447a38e71eddfe640404418a9a836cc2f16d74
04014985229 <04014985229@JIPA-LABIN05-17.ifro.local>
2018-05-14 08:25:19 -0400

    Gravar no banco de dados a partir da tela de cadastro.

--- Grupo7/src/br/edu/ifro/AlunoController.java
+++ Grupo7/src/br/edu/ifro/AlunoController.java
@@ -5,9 +5,16 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.modelo.Aluno;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -16,6 +23,9 @@ import javafx.fxml.Initializable;
  */
 public class AlunoController implements Initializable {
 
+    @FXML
+    private TextField txtnome;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +33,20 @@ public class AlunoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void salvar(ActionEvent event) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula");
+          EntityManager em = emf.createEntityManager ();
+    Aluno aluno1 = new Aluno();
+    aluno1.setNome(txtnome.getText());
+    em.getTransaction().begin();
+    em.persist(aluno1);
+    em.getTransaction().commit();
+
+            }
+    @FXML
+    private void fechar(ActionEvent event) {
+    }
     
 }
