51ce8f724da8861788d67dcd4c520153ceb25290
04014985229 <04014985229@JIPA-LABIN05-17.ifro.local>
2018-05-14 08:29:59 -0400

    Gravar no banco de dados a partir da tela de cadastro. (Ação do botão fechar).

--- Grupo7/src/br/edu/ifro/AlunoController.java
+++ Grupo7/src/br/edu/ifro/AlunoController.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -47,6 +48,8 @@ public class AlunoController implements Initializable {
             }
     @FXML
     private void fechar(ActionEvent event) {
+         Stage stage = (Stage) txtnome.getScene().getWindow();
+    stage.close();
     }
     
 }
